6d62c969c7246293ba5a7a55f616f34ff9d5f791
SamuelTownsend <91908426+SamuelTownsend@users.noreply.github.com>
2023-04-25 20:17:42 -0500

    ADDING COMMENT

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ console.log('Hello World');
 
 let name = 'Sam';
 console.log(name);
-
+// lET ALLOWS FOR VARIABLES TO BE CHANGED LATER IN THE CODE
 let interestRate = 0.3;
 console.log(interestRate);
 interestRate = 1;
-
 console.log(interestRate);
