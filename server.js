9a20934a40bf09292d8d90313fff36827c899e4e
SamuelTownsend <91908426+SamuelTownsend@users.noreply.github.com>
2023-04-29 12:08:30 -0500

    adding code

---
+++ server.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const len = args.length;
+
+if (len === 2) {
+    console.log('No arguments found');
+} else if (len === 3) {
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
+}
